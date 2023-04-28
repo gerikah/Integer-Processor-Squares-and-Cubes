e8956b4215f682df071ac7004ad58f01d0dc5f8f
gerikah <aldaygerikah@gmail.com>
2023-04-28 19:35:42 +0800

    pseudocode and txt files

---
+++ integer_processor.py
@@ -0,0 +1,19 @@
+# Open the input file 'integers.txt' in read mode
+# Read the integers from the file and store them in a list
+# Create two empty lists, even_integers and odd_integers
+# Loop through the list of integers:
+    # If the integer is even, append it to even_integers list
+    # If the integer is odd, append it to odd_integers list
+# Create two empty lists, squares and cubes
+# Loop through the even_integers list:
+    # Calculate the square of each integer and append it to squares list
+# Loop through the odd_integers list:
+    # Calculate the cube of each integer and append it to cubes list
+# Open the first output file 'double.txt' in write mode
+# Loop through the squares list:
+    # Write each square to the file followed by a new line character
+# Close the first output file
+# Open the second output file 'triple.txt' in write mode
+# Loop through the cubes list:
+    # Write each cube to the file followed by a new line character
+# Close the second output file
