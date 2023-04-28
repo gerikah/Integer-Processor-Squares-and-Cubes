e2f12453887ba037ef8fd0662cc63c792006449d
gerikah <aldaygerikah@gmail.com>
2023-04-28 22:03:57 +0800

    update

--- integer_processor.py
+++ integer_processor.py
@@ -1,52 +1,52 @@
-# Open the input file 'integers.txt' in read mode
-with open ("integers.txt", "r") as num_file:
+# open the input file 'integers.txt' in read mode
+with open('integers.txt', 'r') as num_file:
+    # read the integers from the file and store them in a list
+    integers = num_file.read().splitlines()
 
-# Read the integers from the file and store them in a list
-    integers = num_file.read().splitlines
-    
-# Create two empty lists, even_integers and odd_integers
+# convert the list of strings to a list of integers
+integers = list(map(int, integers))
+
+# create two empty lists, even_integers and odd_integers
 even_integers = []
 odd_integers = []
 
-# Loop through the list of integers:
+# loop through the list of integers and append even and odd integers to their respective lists
 for num in integers:
-    
-    # If the integer is even, append it to even_integers list
     if num % 2 == 0:
         even_integers.append(num)
-    # If the integer is odd, append it to odd_integers list
     else:
         odd_integers.append(num)
 
-# Create two empty lists, squares and cubes
-square_even_integers = []
-cube_odd_integers = []
+# create two empty lists, squares and cubes
+squares_even_integers = []
+cubes_odd_integers = []
 
-# Loop through the even_integers list:
+# loop through the even_integers list and calculate the square of each integer
 for num in even_integers:
-    # Calculate the square of each integer and append it to squares list
+    # calculate the square of each integer and append it to squares list
     square = num ** 2
-    square_even_integers.append(square)
+    squares_even_integers.append(square)
 
-# Loop through the odd_integers list:
+# loop through the odd_integers list and calculate the cube of each integer
 for num in odd_integers:
-    # Calculate the cube of each integer and append it to cubes list
+    # calculate the cube of each integer and append it to cubes list
     cube = num ** 3
-    cube_odd_integers.append(cube)
+    cubes_odd_integers.append(cube)
 
-# Open the first output file 'double.txt' in write mode
-with open ("double.txt", "w") as num_file:
+# open the first output file 'double.txt' in write mode
+with open('double.txt', 'w') as num_file:
+    # loop through the squares list and write each square to the file followed by a new line character
+    for square in squares_even_integers:
+        num_file.write(str(square) + '\n')
 
-# Loop through the squares list and write to num_file
-    for square in square_even_integers:
-        num_file.write(str(square) + "\n" )
-# Close the first output file
+# close the first output file
+num_file.close()
 
-# Open the second output file 'triple.txt' in write mode
-with open ("triple.txt", "w") as num_file:
+# open the second output file 'triple.txt' in write mode
+with open('triple.txt', 'w') as num_file:
+    # loop through the cubes list and write each cube to the file followed by a new line character
+    for cube in cubes_odd_integers:
+        num_file.write(str(cube) + '\n')
 
-# Loop through the cubes list and write to num_file
-    for cube in cube_odd_integers:
-        num_file.write(str(square)+ "\n")
-        
-# Close the second output file
+# close the second output file
+num_file.close()
